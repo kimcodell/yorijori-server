5d431bd6a96ff084373fe656e0f9b98a46abcc58
kimcodell <kimminhyuk02@naver.com>
2024-09-12 11:30:17 +0000

    add data query

---
+++ data-query.sql
@@ -0,0 +1,9 @@
+INSERT INTO 
+    yorijori.user (name, nickname, email, passwordHash)
+    VALUES 
+        ("김민혁", "녀기", "test1@naver.com", "123123"),
+        ("박서현", "혀니", "test2@naver.com", "123123"),
+        ("김서현", "현이", "test3@naver.com", "123123"),
+        ("박민혁", "박녁남", "test1@gmail.com", "123123"),
+        ("박수현", "블라불라", "test2@gmail.com", "123123"),
+        ("정은빈", "테스트계정", "test3@gmail.com", "123123");
